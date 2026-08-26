8dfff6d8941bf267a9c8247fc517b07f06eea5e0
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-08-26 15:44:16 +0200

    implemented set_color

--- src/chessGame/ChessPiece.py
+++ src/chessGame/ChessPiece.py
@@ -2,11 +2,14 @@ import Status
 
 class ChessPiece:
     def __init__(self, color):
-        self.color = color
+        self._color = self._set_color(color)
         self.status = Status.alive
 
     def get_color(self):
-        return self.color
+        return self._color
     
     def get_status(self):
         return self.status
+    
+    def _set_color(self, color):
+        self._color = color
