ae99ba01eaa02a9be9ec5d87f6ddd374f280aaed
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-08-26 11:23:05 +0200

    create test_is_free

--- test/ChessCellTest.py
+++ test/ChessCellTest.py
@@ -2,6 +2,8 @@ from src.chessGame.ChessCell import ChessCell
 import unittest
 
 class ChessCellTest(unittest.TestCase):
-    def is_free_test(self):
+    def test_is_free(self):
         c1 = ChessCell()
         self.assertEqual(c1.is_free(), True)
+        c1.set_piece(123)
+        self.assertEqual(c1.is_free(), False)
