78a79f20420e7ded1eb176db14a19f053cfd9b1f
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-08-28 16:14:23 +0200

    create is_white and is_black

--- src/chessGame/ChessPiece.py
+++ src/chessGame/ChessPiece.py
@@ -1,12 +1,22 @@
 from src.chessGame.Status import Status
+from src.chessGame.Color import Color
 
 class ChessPiece:
     def __init__(self, color):
         self._set_color(color)
         self.status = Status.alive
 
-    def get_color(self):
-        return self._color
+    def is_white(self):
+        if(self._color == Color.white):
+            return True
+        else:
+            return False
+    
+    def is_black(self):
+        if(self._color == Color.black):
+            return True
+        else:
+            return False
     
     def get_status(self):
         return self.status
--- src/chessGame/GameChess.py
+++ src/chessGame/GameChess.py
@@ -3,3 +3,4 @@ import numpy as np
 class GameChess:
     def __init__(self):
         self.grid = np.full((8, 8), None)
+        
