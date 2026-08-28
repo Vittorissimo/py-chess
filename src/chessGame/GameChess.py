fb255d60907723e44838f0756a6c5520db116fbb
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-08-28 19:56:56 +0200

    Initialized the GameChess class.

--- src/chessGame/GameChess.py
+++ src/chessGame/GameChess.py
@@ -1,6 +1,17 @@
 import numpy as np
+from src.chessGame.Player import Player
 
 class GameChess:
     def __init__(self):
         self.grid = np.full((8, 8), None)
-        
+        self._player1 = Player()
+        self._player2 = Player()
+    
+    def run(self):
+        pass
+
+    def update(self):
+        pass
+    
+    def init_grid(self):
+        pass
