45d098635007fcde5f3593d6dbba25c32cd059ac
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-09-02 16:46:49 +0200

    implemented control_date and value_modificator

--- src/chessGame/pieces/King.py
+++ src/chessGame/pieces/King.py
@@ -10,31 +10,31 @@ class King(ChessPiece):
     def get_move(self):
         return self.move
     
-    def compute_fisible_set(self, grid):
+    def compute_fisible_set(self, board):
         self._list_move = []
 
-        if(grid[self._cell[0] + self.move][self._cell[1] + self.move] == None):
+        if(board[self._cell[0] + self.move][self._cell[1] + self.move] == None):
             self._list_move.append([self._cell[0] + self.move, self._cell[1] + self.move])
         
-        if(grid[self._cell[0] - self.move][self._cell[1] + self.move] == None):
+        if(board[self._cell[0] - self.move][self._cell[1] + self.move] == None):
             self._list_move.append([self._cell[0] - self.move, self._cell[1] + self.move])
         
-        if(grid[self._cell[0] + self.move][self._cell[1] - self.move] == None):
+        if(board[self._cell[0] + self.move][self._cell[1] - self.move] == None):
             self._list_move.append([self._cell[0] + self.move, self._cell[1] - self.move])
         
-        if(grid[self._cell[0] - self.move][self._cell[1] - self.move] == None):
+        if(board[self._cell[0] - self.move][self._cell[1] - self.move] == None):
             self._list_move.append([self._cell[0] - self.move, self._cell[1] - self.move])
         
-        if(grid[self._cell[0] + self.move][self._cell[1]] == None):
+        if(board[self._cell[0] + self.move][self._cell[1]] == None):
             self._list_move.append([self._cell[0] + self.move, self._cell[1]])
         
-        if(grid[self._cell[0] - self.move][self._cell[1]] == None):
+        if(board[self._cell[0] - self.move][self._cell[1]] == None):
             self._list_move.append([self._cell[0] - self.move, self._cell[1]])
         
-        if(grid[self._cell[0]][self._cell[1] + self.move] == None):
+        if(board[self._cell[0]][self._cell[1] + self.move] == None):
             self._list_move.append([self._cell[0], self._cell[1] + self.move])
         
-        if(grid[self._cell[0]][self._cell[1] - self.move] == None):
+        if(board[self._cell[0]][self._cell[1] - self.move] == None):
             self._list_move.append([self._cell[0], self._cell[1] - self.move])
         
         print(sorted(self._list_move))
@@ -42,3 +42,14 @@ class King(ChessPiece):
     
     def get_fisible_set(self):
         return self._list_move
+    
+    def control_date(self, number):
+        if(number >= 1 or number <= 8):
+            return True
+        
+        return False
+    
+    def value_modificator(self, value_letter, board):
+        key = next(key for key, value in board.letter_dictionary.items() if value == value_letter)
+        print(key)
+        return key
