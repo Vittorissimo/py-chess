5a53702884d44ee74ff031b2901e3b3431548af9
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-09-03 19:50:04 +0200

    king implemented

--- src/chessGame/pieces/King.py
+++ src/chessGame/pieces/King.py
@@ -1,53 +1,41 @@
 from chessGame.pieces.ChessPiece import ChessPiece
+from chessGame.ChessBoard import ChessBoard
+from chessGame.utils.Color import Color
 
 class King(ChessPiece):
     def __init__(self, color):
         super().__init__(color)
-        self.move = 1
-        self._cell = (1, 5)
-        self._list_move = []    
-        
-    def get_move(self):
-        return self.move
+        self.set_cell()
     
-    def compute_fisible_set(self, board):
-        self._list_move = []
+    def compute_feasible_set(self, board: ChessBoard):
+        self._feasible_set.clear()
+        grid_matrix     = board.get_raw_grid()
 
-        if(board[self._cell[0] + self.move][self._cell[1] + self.move] == None):
-            self._list_move.append([self._cell[0] + self.move, self._cell[1] + self.move])
-        
-        if(board[self._cell[0] - self.move][self._cell[1] + self.move] == None):
-            self._list_move.append([self._cell[0] - self.move, self._cell[1] + self.move])
-        
-        if(board[self._cell[0] + self.move][self._cell[1] - self.move] == None):
-            self._list_move.append([self._cell[0] + self.move, self._cell[1] - self.move])
-        
-        if(board[self._cell[0] - self.move][self._cell[1] - self.move] == None):
-            self._list_move.append([self._cell[0] - self.move, self._cell[1] - self.move])
-        
-        if(board[self._cell[0] + self.move][self._cell[1]] == None):
-            self._list_move.append([self._cell[0] + self.move, self._cell[1]])
-        
-        if(board[self._cell[0] - self.move][self._cell[1]] == None):
-            self._list_move.append([self._cell[0] - self.move, self._cell[1]])
-        
-        if(board[self._cell[0]][self._cell[1] + self.move] == None):
-            self._list_move.append([self._cell[0], self._cell[1] + self.move])
-        
-        if(board[self._cell[0]][self._cell[1] - self.move] == None):
-            self._list_move.append([self._cell[0], self._cell[1] - self.move])
-        
-        print(sorted(self._list_move))
-        return None
+        # cicla i e j per riga e colonna
+        # nella matrice grezza (grid_matrix)
+        si, sj = self._cell[0], self._cell[1]
+        for i in range(3):
+            for j in range(3):
+                if((i,j) != (1,1)):
+                    i_d, j_d = si + i - 1, sj + j - 1
+                    if ((i_d >= 0 and j_d >= 0) and (i_d <= 7 and j_d <= 7)):
+                        self._feasible_set.append((i_d, j_d))
+
+        # nemici e amici
+        for i in self._feasible_set:
+            if(grid_matrix[i] != None):
+                if(self._color == (grid_matrix[i].get_color())):
+                    self._feasible_set.remove(i)
     
-    def get_fisible_set(self):
-        return self._list_move
-    
-    def control_date(self, number):
-        if(number >= 1 or number <= 8):
-            return True
-        
-        return False
+    def set_cell(self):
+        if (self._color == Color.white):
+            self._cell = (7, 4)
+        else:
+            self._cell = (0, 4)
+
+    def move(self, cell_move : tuple):
+        self._cell = cell_move
+
     
     def value_modificator(self, value_letter, board):
         key = next(key for key, value in board.letter_dictionary.items() if value == value_letter)
