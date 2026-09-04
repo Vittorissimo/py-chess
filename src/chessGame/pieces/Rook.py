f96cd9484d86ab95769ade5ba2d5748c56960bb8
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-09-04 22:30:26 +0200

    rook implemented

--- src/chessGame/pieces/Rook.py
+++ src/chessGame/pieces/Rook.py
@@ -1,8 +1,36 @@
-import chessGame.pieces.ChessPiece as ChessPiece
+from chessGame.pieces.ChessPiece import ChessPiece
+from chessGame.ChessBoard import ChessBoard
+from chessGame.utils.Color import Color
 
 class Rook(ChessPiece):
-    def __init__(self):
-        self.max_move = 9
+    def __init__(self, number_position, color):
+        super().__init__(color)
+        self.set_cell(number_position)
     
-    def get_max_move(self):
-        return self.max_move
+    def set_cell(self, value):
+        if(value == 1):
+            if (self._color == Color.white):
+                self._cell = (7, 0)
+            else:
+                self._cell = (0, 7)
+        else:
+            if(self._color == Color.white):
+                self._cell = (7, 7)
+            else:
+                self._cell = (0, 0)
+    
+    def move(self, cell_move : tuple):
+        self._cell = cell_move
+    
+    def compute_feasible_set(self, board: ChessBoard):
+        self._feasible_set.clear()
+        grid_matrix = board.get_raw_grid()
+
+        si, sj = self._cell[0], self._cell[1]
+        for i in range(8):
+            if (i != self._cell[0]):
+                self.feasible_set.append(i, sj)
+        
+        for i in range(8):
+            if (i != self._cell[1]):
+                self.feasible_set.append(si, i)
