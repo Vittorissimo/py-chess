13017778491ab20b1e24ee32f3e2ab91bba6a0dd
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-09-14 10:35:37 +0200

    fix compute_feasible_set

--- src/chessGame/pieces/Queen.py
+++ src/chessGame/pieces/Queen.py
@@ -1,8 +1,43 @@
-import chessGame.pieces.ChessPiece as ChessPiece
+from chessGame.pieces.ChessPiece import ChessPiece
+from chessGame.ChessBoard import ChessBoard
 
 class Queen(ChessPiece):
-    def __init__(self):
-        self.max_move = 9
+    def __init__(self, color):
+        super().__init__(color)
     
-    def get_max_move(self):
-        return self.max_move
+    def compute_feasible_set(self, board: ChessBoard):
+        self._feasible_set.clear()
+        grid_matrix = board.get_raw_grid()
+
+        si, sj = self._cell[0], self._cell[1]
+        for i in range(8):
+            if(((si + i) <= 7) and ((sj + i) <= 7) and (i != 0)):
+                self._feasible_set.append((si + i, sj + i))
+        
+        for i in range(8):
+            if(((si - i) >= 0) and ((sj - i) >= 0) and (i != 0)):
+                self._feasible_set.append((si - i, sj - i))
+        
+        for i in range(8):
+            if(((si + i) <= 7) and ((sj - i) >= 0) and (i != 0)):
+                self._feasible_set.append((si + i, sj - i))
+        
+        for i in range(8):
+            if(((si - i) >= 0) and ((sj + i) <= 7) and (i != 0)):
+                self._feasible_set.append((si - i, sj + i))
+        
+        for i in range(8):
+            if (i != self._cell[0]):
+                self._feasible_set.append((i, sj))
+        
+        for i in range(8):
+            if (i != self._cell[1]):
+                self._feasible_set.append((si, i))
+        
+        for i in self._feasible_set:
+            if(grid_matrix[i] != None):
+                if(self._color == (grid_matrix[i].get_color())):
+                    self._feasible_set.remove(i)
+    
+    def move(self, cell_move : tuple):
+        self._cell = cell_move
--- test/PieceTest.py
+++ test/PieceTest.py
@@ -1,12 +1,14 @@
 from chessGame.pieces.King import King
 from chessGame.pieces.Rook import Rook
 from chessGame.pieces.Bishop import Bishop
+from chessGame.pieces.Pawn import Pawn
+from chessGame.pieces.Queen import Queen
 from chessGame.utils.Color import Color
 from chessGame.ChessBoard import ChessBoard
 import numpy as np
 import unittest
 
-class KingTest(unittest.TestCase):
+class PieceTest(unittest.TestCase):
     def test_compute_fisible_set(self):
         board = ChessBoard()
 
@@ -46,11 +48,30 @@ class KingTest(unittest.TestCase):
         # print(r.get_feasible_set())
 
         # Bishop
-        b = Bishop(Color.white, 1)
-        b.move((3, 3))
-        b.compute_feasible_set(board)
-        print(b.get_feasible_set())
+        # b = Bishop(Color.white, 1)
+        # b.move((3, 3))
+        # b.compute_feasible_set(board)
+        # print(b.get_feasible_set())
 
+        # todo
+        # Pawn
+        # pw = Pawn(Color.white)
+        # pb = Pawn(Color.black)
+        # pw.move((5, 5))
+        # pb.move((6, 5))
+        # board.set_raw_grid((5, 5), pw)
+        # board.set_raw_grid((5, 6), pb)
+        # pw.compute_feasible_set(board)
+        # # pb.compute_feasible_set(board)
+        # print("p-white: ", pw.get_feasible_set())
+        # # print("p-black: ", pb.get_feasible_set())
+
+
+        #Queen
+        q = Queen(Color.white)
+        q.move((3, 3))
+        q.compute_feasible_set(board)
+        print(q.get_feasible_set())
 
 
         
